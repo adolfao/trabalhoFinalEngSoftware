010627cffb61c8daf0b725a797d04a2a85060373
Isabelle Rosvadoski Nofre <125320066+IsabelleRosvadoskiNofre@users.noreply.github.com>
2026-06-18 21:58:01 -0300

    Delete Codigos/fabricas directory

--- Codigos/fabricas/fabrica_professor.py
+++
@@ -1,8 +0,0 @@
-from Modelos.professor import Professor
-
-class FabricaProfessor:
-    @staticmethod
-    def criar_professor(id_prof: int, nome: str, disponibilidade: dict) -> Professor:
-        if not nome or len(nome.strip()) < 3:
-            raise ValueError("Nome do professor inválido (mínimo 3 caracteres).")
-        return Professor(id_prof, nome, disponibilidade)
